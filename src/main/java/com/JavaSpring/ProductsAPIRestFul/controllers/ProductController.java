9db6f1d2bf74acfea7b7b50dd1e0067679c28d03
edudias1972 <edudias1972@gmail.com>
2023-09-12 16:40:13 -0300

    Requesito GETAll - findAlll e GETONE - findById

--- src/main/java/com/JavaSpring/ProductsAPIRestFul/controllers/ProductController.java
+++ src/main/java/com/JavaSpring/ProductsAPIRestFul/controllers/ProductController.java
@@ -8,9 +8,11 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 /**
  * Controller Metodos post , get , put , delete
@@ -26,5 +28,26 @@ public class ProductController {
         BeanUtils.copyProperties(productRecordDto, productModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
     }
+
+    /**
+     * findAll() lista todos os produtos.
+     */
+    @GetMapping("/products")
+    public ResponseEntity<List<ProductModel>> getallProducts() {
+        return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
+    }
+
+    /**
+     * Lista apenas um produto por ID
+     */
+    @GetMapping("/products/{id}")
+    public ResponseEntity<Object> getOneProduct(@PathVariable(value = "id") UUID id) {
+        Optional<ProductModel> productO = productRepository.findById(id);
+        if (productO.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(productO.get());
+    }
+
 }
 
