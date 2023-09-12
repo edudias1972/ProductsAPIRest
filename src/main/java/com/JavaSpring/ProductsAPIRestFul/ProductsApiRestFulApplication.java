8f4a787c7f691c4f542ce65f0be612e0bc72a264
edudias1972 <edudias1972@gmail.com>
2023-09-12 03:23:08 -0300

    Iniciando projeto

---
+++ src/main/java/com/JavaSpring/ProductsAPIRestFul/ProductsApiRestFulApplication.java
@@ -0,0 +1,13 @@
+package com.JavaSpring.ProductsAPIRestFul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsApiRestFulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsApiRestFulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JavaSpring/ProductsAPIRestFul/ProductsApiRestFulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JavaSpring.ProductsAPIRestFul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApiRestFulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
